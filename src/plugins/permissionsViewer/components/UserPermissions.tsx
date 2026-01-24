01e982edefe1d7a4ff0cf5afc090df1a46fd29f0
- <120268595+pluckerpilple@users.noreply.github.com>
2026-01-24 03:39:48 +0300

    <>

--- src/plugins/permissionsViewer/components/UserPermissions.tsx
+++ src/plugins/permissionsViewer/components/UserPermissions.tsx
@@ -36,7 +36,6 @@ interface UserPermission {
 }
 
 type UserPermissions = Array<UserPermission>;
-
 const RoleClasses = findCssClassesLazy("role", "roleName", "roleRemoveButton", "roleNameOverflow", "root");
 const RoleBorderClasses = findCssClassesLazy("roleCircle", "dot", "dotBorderColor");
 
@@ -101,10 +100,10 @@ function UserPermissionsComponent({ guild, guildMember, closePopout }: { guild: 
                 permissions: Object.values(PermissionsBits).reduce((prev, curr) => prev | curr, 0n)
             });
 
-            const OWNER = getIntlMessage("GUILD_OWNER") ?? "Server Owner";
+            const OWNER = getIntlMessage("GUILD_OWNER") ?? "Owner";
             userPermissions.push({
                 permission: OWNER,
-                roleName: "Owner",
+                roleName: "(Owner)",
                 roleColor: "var(--primary-300)",
                 rolePosition: Infinity
             });
